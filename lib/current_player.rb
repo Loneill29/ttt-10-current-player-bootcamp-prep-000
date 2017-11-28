a31367ed39325569f67153f0c9cf8b5aa91f4b48
Linda ONeill <oneill.linda21@gmail.com>
2017-11-28 22:35:50 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+  count = 0
+  board.each do |move|
+    if move == "X" || move == "O"
+      count += 1
+  
+  end
+end
+  count
+end
+
+def current_player(board)
+  count = 0
+  if count % 2 == 0
+    print "X"
+  else print "O"
+  end
